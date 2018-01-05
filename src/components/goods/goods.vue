eff0cd2d618f53bc0ae0d06fb5bf340c895a05d9
LXQ <515835543@qq.com>
2018-01-05 21:39:39 +0800

    fixed bugs
    Signed-off-by: LXQ <515835543@qq.com>

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,9 +1,69 @@
 <template>
-  <div>goods</div>
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text border-1px">
+            <span v-show="item.type>0" class="icon" v-bind:class="classMap[item.type]"></span>
+            {{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item">
+              <div class="icon">
+                <img wifth="57" height="57" :src="food.icon" alt="">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default{};
+  const ERROR_OK = 0;
+  export default{
+    props:{
+      seller:{
+        type:Object
+      }
+    },
+    data(){
+      return{
+        goods:[]
+      }
+    },
+    created(){
+      this.$http.get('/api/goods').then((response)=>{
+        response = response.body;
+        if(response.errno === ERROR_OK){
+          this.goods = response.data;
+          console.log(this.goods);
+        }
+      });
+      this.classMap = ['decrease','discount','special','invoice','guanantee']
+    }
+
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -30,19 +30,40 @@
     <div class="background">
       <img v-bind:src="seller.avatar" alt="" width="100%" height="100%">
     </div>
-    <div class="detail" v-show="detailShow">
-      <div class="detail-wrapper clearfix">
-        <div class="detail-main">
-          <h1 class="name">{{seller.name}}</h1>
-          <div class="star-wrapper">
-            <star :size="48" :score="seller.score"></star>
+    <transition name="fade">
+      <div class="detail" v-show="detailShow">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item,index) in seller.supports">
+                <span class="icon" v-bind:class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
           </div>
         </div>
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon-close"></i>
+        </div>
       </div>
-      <div class="detail-close">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -63,6 +84,9 @@
       showDetail(){
         this.detailShow = true
         console.log(this)
+      },
+        hideDetail(){
+        this.detailShow = false
       }
     },
     created() {
@@ -277,7 +301,7 @@
         position: relative
         width: 32px
         height: 32px
-        margin: 64px auto 0 auto
+        margin: -64px auto 0 auto
         clear: both
         font-size: 32px
 </style>
